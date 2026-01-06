25a1f096f73d8d29372839ad731aab88f4c0b662
raushanbyteiq-new <raushanbyteiq@gmail.com>
2026-01-06 04:24:09 -0800

    Update useSpeechToText.ts

--- Group_meeting/src/hooks/useSpeechToText.ts
+++ Group_meeting/src/hooks/useSpeechToText.ts
@@ -3,7 +3,7 @@ import type { Room, LocalParticipant } from 'livekit-client';
 import { Track } from 'livekit-client';
 
 // ⚠️ REPLACE THIS WITH YOUR REAL KEY (The one that worked in the HTML file)
-const DEEPGRAM_API_KEY = "19b2a3f9d9be5a9092dff1810650198119a5d76b"; 
+const DEEPGRAM_API_KEY = ""; 
 
 export const useSpeechToText = (
   room: Room | undefined,
